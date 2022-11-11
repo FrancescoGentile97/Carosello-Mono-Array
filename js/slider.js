04e743110458cda09690cc43f640bad3e7f5b7c0
FrancescoGentile97 <francesco.gentile97@icloud.com>
2022-11-11 14:52:33 +0100

    completed exercise

---
+++ js/slider.js
@@ -0,0 +1,86 @@
+// creo un array dove metto all'interno la lista delle immagini
+const images = [
+    "imgs/01.webp",
+    "imgs/02.webp",
+    "imgs/03.webp",
+    "imgs/04.webp",
+    "imgs/05.webp",
+];
+
+let currentImgIndex = 0;
+
+const sliderContainerEl = document.querySelector(".slider-container");
+let sliderImgEl = "";
+
+const btnNext = document.getElementById("slider-btn-next");
+const btnPrev = document.getElementById("slider-btn-prev");
+const imgsContainerEl = document.querySelector(".imgs-container");
+
+sliderImgEl.src = images[currentImgIndex];
+
+for (let i = 0; i <= images.length - 1; i++) {
+
+    let sliderImgEl = document.createElement("img");
+    sliderImgEl.src = images[i];
+
+    if (i === 0) {
+        sliderImgEl.classList.add("d-block");
+    }
+
+    sliderContainerEl.append(sliderImgEl);
+
+    const imgsEl = document.createElement("img");
+    imgsEl.src = images[i];
+
+    if (i === 0) {
+        imgsEl.classList.add("active");
+    }
+
+    imgsContainerEl.append(imgsEl);
+}
+
+btnNext.addEventListener("click", function () {
+
+    const oldImgEl = document.querySelector(`.slider-container :nth-child(${currentImgIndex + 1})`);
+
+    oldImgEl.classList.remove("d-block");
+
+    const oldImgs = document.querySelector(`.imgs-container :nth-child(${currentImgIndex + 1})`);
+    oldImgs.classList.remove("active");
+
+    currentImgIndex++;
+
+    if (currentImgIndex > images.length - 1) {
+        currentImgIndex = 0;
+    }
+
+    const newImgEl = document.querySelector(`.slider-container :nth-child(${currentImgIndex + 1})`);
+
+    newImgEl.classList.add("d-block");
+
+    const newImgs = document.querySelector(`.imgs-container :nth-child(${currentImgIndex + 1})`);
+    newImgs.classList.add("active");
+})
+
+btnPrev.addEventListener("click", function () {
+
+    const oldImgEl = document.querySelector(`.slider-container :nth-child(${currentImgIndex + 1})`);
+
+    oldImgEl.classList.remove("d-block");
+
+    const oldImgs = document.querySelector(`.imgs-container :nth-child(${currentImgIndex + 1})`);
+    oldImgs.classList.remove("active");
+
+    currentImgIndex--;
+
+    if (currentImgIndex < 0) {
+        currentImgIndex = images.length - 1;
+    }
+
+    const newImgEl = document.querySelector(`.slider-container :nth-child(${currentImgIndex + 1})`);
+
+    newImgEl.classList.add("d-block");
+
+    const newImgs = document.querySelector(`.imgs-container :nth-child(${currentImgIndex + 1})`);
+    newImgs.classList.add("active");
+});
